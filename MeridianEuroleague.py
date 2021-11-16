99316ff01f9965e344b220ebf6b0bb88fa746ffa
Nikola Pejic <pejic.nikola96@outlook.com>
2021-11-16 16:19:36 +0100

    added maps versions

---
+++ MeridianEuroleague.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.by import By
+
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+service = Service("C:\\Users\\Nikola\\Desktop\\SpringScrape\\scrape\\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.get('https://meridianbet.ba/sr/kladjenje/ko%C5%A1arka/poeni-igra%C4%8Da/euroleague')
+rows = driver.find_elements(By.TAG_NAME, 'standard-item-info')
+print(len(rows))
+for row in rows:
+    element = row.find_element(By.CLASS_NAME, 'away')
+    print(element.text)
---
+++ remove_duplicates.py
@@ -0,0 +1,20 @@
+import os.path
+from os import listdir
+from os.path import isfile, join
+
+path = 'maps/Map_MaxBet.txt'
+
+with open(path) as f:
+    lines = f.read().splitlines()
+    players = []
+    for line in lines:
+        if line not in players and line != '':
+            players.append(line)
+    players.sort()
+
+new_path = 'maps/Map_MaxBet.txt'
+new_file = open(new_path, 'w')
+for player in players:
+    new_file.write(player + '\n')
+
+new_file.close()
