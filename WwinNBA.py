315c36b8317a017fc72cfb90564b52ed29a4d60e
AzureAD\NikolaPejic <nikola.pejic@oxynapse.com>
2021-11-25 15:34:40 +0100

    env update

--- WwinNBA.py
+++ WwinNBA.py
@@ -9,7 +9,7 @@ import os
 
 op = webdriver.ChromeOptions()
 op.add_argument('headless')
-dotenv_path = Path('env.env')
+dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 service = Service(os.environ.get('CHROMEDRIVER'))
 driver = webdriver.Chrome(service=service)
