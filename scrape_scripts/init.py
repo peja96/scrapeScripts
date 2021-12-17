b3f08a99b30fbfb9cd087949d2d2443e77c4ed8a
AzureAD\RadeToprek <rade.toprek@oxynapse.com>
2021-12-17 13:49:47 +0100

    Added environment

--- scrape_scripts/init.py
+++ scrape_scripts/init.py
@@ -8,8 +8,6 @@ import os
 def get_driver():
     op = webdriver.ChromeOptions()
     op.add_argument('headless')
-    dotenv_path = Path('../.env')
-    load_dotenv(dotenv_path=dotenv_path)
     service = Service(os.environ.get('CHROMEDRIVER'))
     driver = webdriver.Chrome(service=service)
     return driver
